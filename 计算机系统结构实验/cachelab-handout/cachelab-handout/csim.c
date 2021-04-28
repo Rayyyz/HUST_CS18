b83ad847383bc5ff2910466bce91cf4bc4b177b0
Touko <48713721+Rayyyz@users.noreply.github.com>
2021-04-28 23:38:49 +0800

    计算机系统结构实验，完成

--- 计算机系统结构实验/cachelab-handout/cachelab-handout/csim.c
+++ 计算机系统结构实验/cachelab-handout/cachelab-handout/csim.c
@@ -1,415 +1,244 @@
-//使用双向链表维护LRU特性,双向链表可以使删除节点的复杂度为O（1）
-//使用哈希表保证能够快速访问缓存中数据
+#define _GNU_SOURCE
 #include "cachelab.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "math.h"
-#include "malloc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <string.h>
 
-//定义LRU缓存
-typedef struct LRUCache
-{
-    int groupNum; //缓存有几组
-    int lineNum;  //每组有几行
-    Group **groups;
-} LRUCache;
+typedef struct Line{
+  int age;       //距离上次访问时长
+  int valid;     //有效位（是否有数据使用缓存
+  int tag;      //组号
+  int* block; 	//块数据
+} Line;
 
-//缓存组
-typedef struct Group
-{
-    int tag;         //组号
-    int lineNum;     //行数
-    int curSize;     //实际节点个数
-    Block **hashMap; //哈希表
-    Block *linkHead; //链表头
-    Block *linkTail; //链表尾
+typedef struct Group{
+  Line* lines; // 每一组有多少行
 } Group;
 
-//缓存单元
-typedef struct Block
-{
-    int tag;   //组号
-    long data; //数据
+typedef struct Cache{
+  int groupNum;    // 每个cache有多少组
+  int lineNum;		//每个组有多少行
+  Group* groups;
+} Cache;
 
-    struct Block *LinkPrev; //链表前一个元素
-    struct Block *LinkNext; //链表后一个元素
-    struct Block *HashPrev; //哈希链表前一个元素
-    struct Block *HashNext; //哈希链表后一个元素
-} Block;
+Cache *createCache(int groupNum, int lineNum);
+Line createLine(int tag, int blockSize, int memAddr);
+void freeCache(Cache* cache);
+int calculateAddr(char* fileLine, long* memAddr);
+void incAge(Group* group, int linenum);
+void visitData(Group* group, int linenum, int tag, int blockSize, long memAddr,int* hit, int* miss, int* eviction);
+void execCache(int s, int E, int b, FILE* fp, int v,int* hit, int* miss, int* eviction);
+void help();
 
-//函数声明
-LRUCache *createLRUCache(int groupnum, int linenum);          //创建缓存
-Block *createBlock(int groupnum, long data);                  //创建缓存单元
-Block *deleteLinkBlock(LRUCache *cache, Block *delBlock);     //删除链表缓存单元
-int insertLinkBlock(LRUCache *cache, Block *newBlock);        //插入链表缓存单元
-Block *findBlock(LRUCache *cache, long data);                 //查询缓存单元
-void insertHashBlock(LRUCache *cache, Block *newBlock);       //插入哈希表缓存单元
-void deleteHashBlock(LRUCache *cache, Block *delBlock);       //删除哈希表缓存单元
-void updateLRUCache(LRUCache *cache, Block *b);               //更新缓存
-int visitData(LRUCache *cache, long data);                    //在缓存内访问数据
-void printCache(LRUCache *cache);                             //打印缓存数据
-void help();                                                  //帮助文档
-void calculateAddr(char *fileLine, long *memAddr, int *size); //获得内存地址和字节数
-void execCache(int v, int s, int E, int b, FILE *fp, int *hit, int *miss, int *eviction);
-void accessData(LRUCache *cache, long addr, int *hit, int *miss, int *eviction); //访问数据，统计命中率
-
-int main(int argc, char **argv)
-{
-    int hit_count = 0, miss_count = 0, eviction_count = 0;
-    int v, s, E, b, t;
-    FILE *fp;
-
-    for (int i = 1; i < argc; i++)
+int main(int argc, char** argv){
+    int hit, miss, eviction;
+    int v=0,s, E, b, i;
+    FILE* fp; 
+    
+    for (i = 1; i < argc; i++)
     {
         char *arg = *(argv + i);
         char *nextarg = *(argv + i + 1);
-        if (strcmp(arg, "-v") == 0)
-        {
+        if (strcmp(arg, "-v") == 0){
             v = 1;
         }
-        else if (strcmp(arg, "-s") == 0)
-        {
+        else if (strcmp(arg, "-s") == 0){
             s = atoi(nextarg);
             i++;
         }
-        else if (strcmp(arg, "-E") == 0)
-        {
+        else if (strcmp(arg, "-E") == 0){
             E = atoi(nextarg);
             i++;
         }
-        else if (strcmp(arg, "-b") == 0)
-        {
+        else if (strcmp(arg, "-b") == 0){
             b = atoi(nextarg);
             i++;
         }
-        else if (strcmp(arg, "-t") == 0)
-        {
+        else if (strcmp(arg, "-t") == 0){
             fp = fopen(nextarg, "r");
             i++;
         }
-        else if (strcmp(arg, "-h") == 0)
-        {
+        else if (strcmp(arg, "-h") == 0){
             help();
         }
     }
-    if (s == 0 || E == 0 || b == 0 || fp == NULL)
-    {
-        printf("缺少必要的参数。");
-        help();
-        exit(1);
-    }
-    execCache(v, s, E, b, fp, &hit_count, &miss_count, &eviction_count);
-    printSummary(hit_count, miss_count, eviction_count);
+    execCache(s, E, b, fp, v, &hit, &miss, &eviction);  
+    printSummary(hit, miss, eviction);
     return 0;
 }
-
-//操作数据
-void accessData(LRUCache *cache, long addr, int *hit, int *miss, int *eviction)
-{
-    int tag = (int)(addr % cache->groupNum);
-    Group *group = cache->groups[tag];
-    for (int i = 0; i < cache->lineNum; i++)
-    {
-        int ret = visitData(cache, addr);
-        if (ret == 1) //假设命中
-            *hit += 1;
-        else if (group->curSize == cache->lineNum)
-        { //未命中且缓存已满
-            *miss += 1;
-            *eviction += 1;
-        }
-        else
-        { //未命中且缓存不满
-            *miss += 1;
-        }
-    }
+//创建cache
+Cache *createCache(int groupNum, int lineNum){
+	Cache* cache = (Cache*) malloc(sizeof(Cache));
+	cache->groupNum = groupNum;
+	cache->lineNum = lineNum;
+  	Group* groups = (Group*)malloc(groupNum * sizeof(Group));
+  	cache->groups = groups;
+  	for(int i = 0; i < groupNum; i++){
+      		Line* lines = (Line*) malloc(lineNum * sizeof(Line));
+      		for(int j = 0; j < lineNum; j++){
+          		lines[j].valid = 0;
+      		}
+      		groups[i].lines = lines;
+  	}
+  	return cache;
 }
 
-//执行缓存测试
-void execCache(int v, int s, int E, int b, FILE *fp, int *hit, int *miss, int *eviction)
-{
-    int groupnum = pow(2, s);
-    int blocksize = pow(2, b);
-    LRUCache *cache = createLRUCache(groupnum, E);
+//创建行
+Line createLine(int tag, int blockSize, int memAddr){
+    Line line;
+    line.valid = 1;
+    line.age = -1;
+    line.tag = tag;
+    int* block = (int*) malloc(blockSize * sizeof(int));//根据块大小来定
+    int byteNum = memAddr & (blockSize - 1);		//计算地址字节数
+    for(int i = 0; i < blockSize; i++){
+        block[i] = memAddr - byteNum + i;
+    }
+    line.block = block;
+    return line;
+}
 
-    char *fileLine = NULL; //文件行指针
-    size_t len = 0;        //每行的长度
+//释放Cache
+void freeCache(Cache* cache){
+    for(int i = 0; i < cache->groupNum; i++){
+        for(int j = 0; j < cache->lineNum; j++){
+            free(cache->groups[i].lines[j].block);
+        }
+        free(cache->groups[i].lines);
+    }
+    free(cache->groups);
+    free(cache);
+    return;
+}
+    
+//计算出要访问的主存地址以及地址的长度（字节为单位ie
+int calculateAddr(char* fileLine, long* memAddr){
+    int addrlen = 0;
+    while(fileLine[3 + addrlen] != ','){
+        addrlen++;
+    }
+    char* addr = (char*) malloc(addrlen * sizeof(char));
+    int i = 0;
+    do{
+        addr[i] = fileLine[i + 3];
+        i++;
+    } while(i < addrlen);
+    *(addr + addrlen) = '\0';
+    *memAddr = strtol(addr, NULL, 16);
+    free(addr);
+    return addrlen;
+}
+
+//将没有命中的行的age++ 
+void incAge(Group* group, int linenum){
+    for(int i = 0; i < linenum; i++){
+        if(group->lines[i].valid == 1){
+            group->lines[i].age += 1;
+        }
+    }
+    return;
+}
+
+//计算Hit/Miss/Eviction
+void visitData(Group* group,int linenum, int tag, int blockSize, long memAddr,int* hit, int* miss, int* eviction){
+    int InvalidLine = -1;
+    int evictionLine = -1;
+    int max_age = 0;
+    for(int i = 0; i < linenum; i++){
+        if((group->lines[i].valid == 1) && (group->lines[i].tag == tag)){	//区号相同，有效
+            *hit +=1;	//命中
+            group->lines[i].age = -1;	
+            incAge(group, linenum);
+            return ;
+        } else if((InvalidLine == -1) && (group->lines[i].valid == 0)){		//未命中，记录空闲行
+            InvalidLine = i;	
+        } else if((group->lines[i].valid == 1) && (group->lines[i].age >= max_age)){	//都不空闲，选出age最大的并替换
+            evictionLine = i;	
+            max_age = group->lines[i].age;
+        }
+    }
+    *miss += 1;
+    if(InvalidLine != -1){
+        group->lines[InvalidLine] = createLine(tag, blockSize, memAddr);
+        incAge(group, linenum);
+    } else{		//把age最大的替换
+        *eviction += 1;	
+        free(group->lines[evictionLine].block);
+        group->lines[evictionLine] = createLine(tag, blockSize, memAddr);
+        incAge(group,linenum);
+    }
+}
+                       
+//执行Cache测试
+void execCache(int s, int E, int b, FILE* fp, int v,int* hit, int* miss, int* eviction){
+    *hit = 0;
+    *miss = 0;
+    *eviction = 0;
+    int groupnum = pow(2,s);
+    int blockSize = pow(2,b);
+    Cache* cache = createCache(groupnum, E);
+
+    char* fileLine = NULL;//文件行指针
+    size_t len = 0; //每一个line的长度
     ssize_t read;
 
-    while (read = getline(&fileLine, &len, fp) != -1)
-    { //逐行分析
-        char *printLine = fileLine;
-        printLine[strlen(printLine) - 1] = "\0";
-        printf("%s", printLine);
-        if (fileLine[0] == 'I') //忽略I指令
-            continue;
-
-        long addr;                             //内存地址
-        char op = fileLine[1];                 //指令，M，L，S
-        int size;                              //访问的内存字节数量
-        calculateAddr(fileLine, &addr, &size); //得到内存地址和访问的字节数量
-
+    while((read = getline(&fileLine, &len, fp)) != -1){//逐行解析
+		if(v==1)
+		{
+			char* printLine = fileLine;
+			printLine[strlen(printLine) - 1] = '\0';
+			printf("%s", printLine);
+		}
+        if(fileLine[0] == 'I') {continue;}
+        long memAddr;                          //内存地址
+		char op = fileLine[1];                 //指令，M，L，S
+		//int size;                            //可忽略。访问的内存字节数量
+		int addrlen = calculateAddr(fileLine, &memAddr); //得到内存地址和访问的字节数量
+	//组相连地址构成为 addr = （区号tag+组号groupTag-s+块内地址blockSize-b）
+        int groupTag = (memAddr >> b) & (groupnum - 1);
+        Group* group = &(cache->groups[groupTag]); 
+        int maxTag = pow(2,(4 * addrlen) - s - b) - 1;
+        int tag = (memAddr >> (s + b)) & maxTag;
         switch (op)
-        {
-        case 'M':
-            //数据修改
-            accessData(cache, addr, hit, miss, eviction);
-            accessData(cache, addr, hit, miss, eviction);
-            break;
-        case 'S':
-            //数据存储
-            accessData(cache, addr, hit, miss, eviction);
-            break;
-        case 'L':
-            //数据装载
-            accessData(cache, addr, hit, miss, eviction);
-            break;
-        default:
-            printf("不能识别到S/M/L指令");
-            break;
-        }
-    }
-}
-
-//计算出一行中要访问的主存地址以及字节长度
-void calculateAddr(char *fileLine, long *memAddr, int *size)
-{
-    int addrlen = 0;
-    while (fileLine[3 + addrlen] != ',')
-        addrlen++;
-    char *addr = (char *)malloc(addrlen * sizeof(char));
-    int i = 0;
-    do
-    {
-        addr[i] = fileLine[i + 3]; //地址位
-        i++;
-    } while (i < addrlen);
-    *(addr + addrlen) = '\0';          //拼接字符串
-    *memAddr = strtol(addr, NULL, 16); //将地址由char数组转换成16进制数
-    free(addr);
-}
-
-//创建缓存
-LRUCache *createLRUCache(int groupnum, int linenum)
-{
-    LRUCache *cache = (LRUCache *)malloc(sizeof(LRUCache));
-    cache->groupNum = groupnum;
-    cache->lineNum = linenum;
-    for (int i = 0; i < groupnum; i++)
-    {
-        cache->groups[i] = (Group *)malloc(sizeof(Group));
-        cache->groups[i]->curSize = 0;
-        cache->groups[i]->hashMap = (Block *)malloc(sizeof(Block) * linenum);
-        cache->groups[i]->lineNum = linenum;
-        cache->groups[i]->linkHead = NULL;
-        cache->groups[i]->linkTail = NULL;
-        cache->groups[i]->tag = i;
-    }
-    return cache;
-}
-
-//创建缓存单元
-Block *createBlock(int groupnum, long data)
-{
-    Block *block = (Block *)malloc(sizeof(Block));
-
-    block->data = data;
-    block->tag = (int)(data & (groupnum - 1));
-    block->HashPrev = NULL;
-    block->HashNext = NULL;
-    block->LinkNext = NULL;
-    block->LinkPrev = NULL;
-    return block;
-}
-
-/*-----链表函数实现-----*/
-//删除节点,返回已删除的节点
-Block *deleteLinkBlock(LRUCache *cache, Block *delBlock)
-{
-    int tag = delBlock->tag;
-    Group *group = cache->groups[tag];
-    if (group->curSize == 0)
-        return;
-    if (group->curSize == 1)
-    { //假设链表只剩当前一个节点
-        group->linkHead = group->linkTail = NULL;
-    }
-    else if (group->linkHead == delBlock)
-    { //假设删除的是头节点
-        group->linkHead = delBlock->LinkNext;
-        group->linkHead->LinkPrev = NULL;
-    }
-    else if (group->linkTail == delBlock)
-    { //假设删除的是尾节点
-        group->linkTail = delBlock->LinkPrev;
-        group->linkTail->LinkNext = NULL;
-    }
-    else
-    { //删除的节点在中间
-        delBlock->LinkPrev->LinkNext = delBlock->LinkNext;
-        delBlock->LinkNext->LinkPrev = delBlock->LinkPrev;
-    }
-    group->curSize--;
-    return delBlock;
-}
-
-//插入链表,返回是否有淘汰
-int insertLinkBlock(LRUCache *cache, Block *newBlock)
-{
-    int evic = 0;
-    int tag = newBlock->tag;
-    Group *group = cache->groups[tag];
-    if (group->curSize == cache->lineNum)
-    { //如果缓存容量已满，则淘汰表尾的数据
-        deleteLinkBlock(group, group->linkTail);
-        evic = 1;
-    }
-    if (group->curSize == 0)
-    { //如果链表为空
-        group->linkHead = group->linkTail = newBlock;
-    }
-    else
-    { //插入表头
-        newBlock->LinkNext = group->linkHead;
-        newBlock->LinkPrev = NULL;
-        group->linkHead->LinkPrev = newBlock;
-        group->linkHead = newBlock;
-    }
-    group->curSize++;
-    return evic;
-}
-
-/*-----哈希表函数实现-----*/
-
-//利用hashMap查找对应数据
-Block *findBlock(LRUCache *cache, long data)
-{
-    int tag = (int)(data & (cache->groupNum - 1));
-    int pos = (int)(data & (cache->lineNum - 1));
-    Group *group = cache->groups[tag];
-    if (group->hashMap[pos])
-    {
-        Block *b = group->hashMap[pos];
-        while (b)
-        {
-            if (b->data == data)
-                return b;
-            b = b->HashNext;
-        }
-    }
-    return NULL;
-}
-
-//插入数据-哈希表
-void insertHashBlock(LRUCache *cache, Block *newBlock)
-{
-    int tag = (int)((newBlock->data) & (cache->groupNum - 1));
-    int pos = (int)((newBlock->data) & (cache->lineNum - 1));
-    Group *group = cache->groups[tag];
-    Block *b = group->hashMap[pos];
-
-    if (b != NULL)
-    { //如果当前槽内已有其他数据项，则另新增项的hashNext为已有项
-        newBlock->HashNext = b;
-        b->HashPrev = newBlock;
-    }
-    //将数据放入哈希表
-    group->hashMap[pos] = newBlock;
-}
-
-//删除数据-哈希表
-void deleteHashBlock(LRUCache *cache, Block *delBlock)
-{
-    int tag = (int)((delBlock->data) & (cache->groupNum - 1));
-    Group *group = cache->groups[tag];
-    if (group->curSize == 0)
-        return;
-
-    int pos = (int)((delBlock->data) & (cache->lineNum - 1));
-    Block *b = group->hashMap[pos];
-    while (b)
-    {
-        if (b->data == delBlock->data)
-        {
-            if (b->HashPrev == NULL)
-            { //如果b为链表第一个节点
-                group->hashMap[pos] = b->HashNext;
-                group->hashMap[pos]->HashPrev = NULL;
-            }
-            else
-            { //b不为链表第一个节点
-                b->HashPrev->HashNext = b->HashNext;
-            }
-            if (b->HashNext)
-            { //b不为链表最后一个节点
-                b->HashNext->HashPrev = b->HashPrev;
-            }
-        }
-        b = b->HashNext;
-    }
-}
-
-//更新Cache，如果访问到到链表中已存在的数据，将其更新到链表表头（哈希表不需要变化）
-void updateLRUCache(LRUCache *cache, Block *b)
-{
-    deleteLinkBlock(cache, b);
-    insertLinkBlock(cache, b);
-}
-
-//命中返回1,miss返回0
-int visitData(LRUCache *cache, long data)
-{
-    //利用哈希表查找
-    Block *b = findBlock(cache, data);
-    //数据已经在缓存中，更新数据并返回true
-    if (b != NULL)
-    {
-        updateLRUCache(cache, b);
-        return 1;
-    }
-    //数据不在缓存中，需要新建缓存插入链表表头
-    Block *newBlock = createBlock(cache->groupNum, data);
-    int tag = (int)(data & (cache->groupNum - 1));
-    Group *group = cache->groups[tag];
-    deleteHashBlock(cache, group->linkTail);
-    insertHashBlock(cache, newBlock);
-    insertLinkBlock(cache, newBlock);
-
-    return 0;
-}
-
-//打印Cache中的内容
-void printCache(LRUCache *cache)
-{
-    if (cache == NULL)
-        return;
-    printf("LRUCache\ngroups:%d\nlines:%d\n", cache->groupNum, cache->lineNum);
-    for (int i = 0; i < cache->groupNum; i++)
-    {
-        printf("Tag:%d\n", i);
-        Group *group = cache->groups[i];
-        Block *b = group->linkHead;
-        while (b)
-        {
-            printf("%d\n", b->data);
-            b = b->LinkNext;
-        }
-    }
+		{
+		case 'M':
+			//数据修改
+			visitData(group,E, tag, blockSize, memAddr, hit, miss, eviction);
+			visitData(group,E, tag, blockSize, memAddr, hit, miss, eviction);
+			break;
+		case 'S':
+			//数据存储
+			visitData(group,E, tag, blockSize, memAddr, hit, miss, eviction);
+			break;
+		case 'L':
+			//数据装载
+			visitData(group,E, tag, blockSize, memAddr, hit, miss, eviction);
+			break;
+		default:
+		    printf("不能识别到S/M/L指令");
+		    break;
+		}
+	}
+      
+		fclose(fp);
+		free(fileLine);
+		freeCache(cache);
+	
 }
 
 //帮助文档
-void help()
-{
+void help(){
     printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
-           "Options:\n"
-           "\t-h\tPrint this help message.\n"
-           "\t-v\tOptional v flag.\n"
-           "\t-s <num>\tNumber of group index bits.\n"
-           "\t-E <num>\tNumber of lines per group.\n"
-           "\t-b <num>\tNumber of block offgroup bits.\n"
-           "\t-t <file>\tTrace file.\n\n"
-           "Examples:\n"
-           "\tlinux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.file\n"
-           "\tlinux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.file\n");
+            "Options:\n"
+            "\t-h\tPrint this help message.\n"
+            "\t-v\tOptional v flag.\n"
+            "\t-s <num>\tNumber of group index bits.\n"
+            "\t-E <num>\tNumber of lines per group.\n"
+            "\t-b <num>\tNumber of block offgroup bits.\n"
+            "\t-t <file>\tTrace file.\n\n"
+            "Examples:\n"
+            "\tlinux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.file\n"
+            "\tlinux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.file\n");
 }
